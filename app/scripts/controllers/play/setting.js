21ac045a54f663d5d508824a9595eaea6eb4a93b
taotao <ttao@thoughtworks.com>
2015-10-31 23:01:02 +0800

    [taotao] 增加可以设置一次性抽多人的图标

--- app/scripts/controllers/play/setting.js
+++ app/scripts/controllers/play/setting.js
@@ -1,11 +1,53 @@
 'use strict';
+
 angular.module('app').controller('PlaySettingCtrl', function SettingCtrl(daoLottery) {
   var vm = this;
   vm.lottery = daoLottery;
 
-  vm.onContinue = function() {
-    daoLottery.awards = _.filter(daoLottery.awards, function(award){
+  vm.onContinue = function () {
+    daoLottery.awards = _.filter(daoLottery.awards, function (award) {
       return !!award.name || !!award.count || !!award.item;
     });
+  };
+
+  vm.showDialog = function () {
+    setConcurrentHits.value = localStorage.getItem('concurrentHits') || 1;
+    bootbox.prompt(setConcurrentHits);
+  };
+
+
+  function isNumber(string) {
+    return /^\d+$/.test(string);
+  }
+
+  function isConcurrentHitsValid (result) {
+    return !isNumber(result) || parseInt(result) > 3 || parseInt(result) < 1;
+  }
+
+  var setConcurrentHits = {
+    size: 'small',
+    title: '设定每次抽取人数:',
+    value: null,
+    callback: function (result) {
+      if(result === null) {
+        return;
+      }
+
+      if (isConcurrentHitsValid(result)) {
+        bootbox.alert(warningAndForceAgain);
+      }
+      else {
+        localStorage.setItem('concurrentHits', result);
+      }
+    }
+  };
+
+  var warningAndForceAgain = {
+    size: 'small',
+    title: '警告',
+    message: '只能输入1~3之间的数字!',
+    callback: function () {
+      vm.showDialog();
+    }
   };
 });
